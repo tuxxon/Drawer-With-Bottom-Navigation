2eebb464076b44bbca1156c7da37721723280ca6
Gordon Ahn <tuxxon@nate.com>
2019-12-12 14:20:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Drawer-With-Bottom-Navigation
+Navigation Drawer with bottom navigation
+
+1. This is an android app with navigation drawer including bottom navigation in java.
+
+<img src="screenshots/Screenshot_1576126380.png" height="30%" width="30%">
+<img src="screenshots/Screenshot_1576126369.png" height="30%" width="30%">
+<img src="screenshots/Screenshot_1576126376.png" height="30%" width="30%">   
---
+++ app/src/main/java/com/touchizen/drawerwithbottomnavigation/MainActivity.java
@@ -0,0 +1,121 @@
+package com.touchizen.drawerwithbottomnavigation;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private NavController navController;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initFab();
+        initNavigation();
+        //showBottomNavigation(false);
+    }
+
+    private void initToolbar() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    private void initFab() {
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    private void initNavigation() {
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        bottomNavView = findViewById(R.id.bottom_nav_view);
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send,
+                R.id.bottom_home, R.id.bottom_dashboard, R.id.bottom_notifications)
+                .setDrawerLayout(drawer)
+                .build();
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(bottomNavView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+
+    private void showBothNavigation(boolean isShowable) {
+        navigationView.setVisibility(isShowable? View.VISIBLE : View.GONE);
+        bottomNavView.setVisibility(isShowable? View.VISIBLE : View.GONE);
+    }
+
+    private void showBottomNavigation(boolean isShowable) {
+        bottomNavView.setVisibility(isShowable? View.VISIBLE : View.GONE);
+    }
+
+}
